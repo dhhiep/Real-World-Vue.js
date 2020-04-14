87204f90e5299d60d58c61767512c04f297a3990
Hiep Dinh <hoanghiepitvnn@gmail.com>
2020-04-14 17:49:12 +0700

    Lesson 8 - Slots: Add default slot to BaseIcon

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -6,10 +6,9 @@
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <BaseIcon name="users" />
-      <span>{{ event.attendees.length }} attending</span>
-    </div></router-link
-  >
+      <BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon>
+    </div>
+  </router-link>
 </template>
 
 <script>
